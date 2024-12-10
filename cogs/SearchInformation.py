80109e4710263b07d99ec53796bac48e3ccceccb
RRRGR <82594900+RRRGR@users.noreply.github.com>
2024-12-10 09:53:53 +0900

    add command showing chart image

--- cogs/SearchInformation.py
+++ cogs/SearchInformation.py
@@ -8,6 +8,12 @@ from discord import app_commands
 from discord.ext import commands
 from discord.ext.commands import Bot, Context
 from typing import List
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from io import BytesIO
 
 from api.api import MusicGameBotAPI
 from MusicGameBot import PATH_TO_IMAGE
@@ -48,6 +54,35 @@ class SearchInformation(commands.GroupCog, name="song"):
             )
 
     @app_commands.command()
+    @app_commands.describe(title="曲名", game="機種名", artist="作曲者等")
+    async def image(
+        self,
+        interaction: discord.Interaction,
+        title: str,
+        game: str | None,
+        artist: str | None,
+    ):
+        """Search Songs"""
+        await interaction.response.defer()
+        result = self.api.search_songs(title, game, artist)
+        num_songs = result["total"]
+        if num_songs == 0:
+            embed = discord.Embed(
+                title=title,
+                description="No results found.",
+                color=discord.Colour.blue(),
+            )
+            await interaction.followup.send(embed=embed)
+        elif num_songs == 1:
+            await interaction.followup.send(
+                view=DropdownView(DifficultyDropdown(result["songs"]))
+            )
+        else:
+            await interaction.followup.send(
+                view=DropdownView(SongsDropdownForImage(result["songs"]))
+            )
+
+    @app_commands.command()
     @app_commands.describe(game="機種名", level="レベル (e.g. 20, 14+)")
     async def gacha(
         self,
@@ -70,6 +105,7 @@ class SearchInformation(commands.GroupCog, name="song"):
 
     @search.autocomplete("game")
     @gacha.autocomplete("game")
+    @image.autocomplete("game")
     async def game_title_autocomplete(
         self,
         interaction: discord.Interaction,
@@ -168,6 +204,106 @@ class SongsDropdown(discord.ui.Select):
         )
         file, embed = make_embed(result["songs"][0])
         await interaction.message.edit(embed=embed, attachments=[file], view=None)
+
+
+class SongsDropdownForImage(discord.ui.Select):
+    def __init__(
+        self,
+        song_list: list,
+    ):
+        self.song_list = song_list
+        options = []
+        for i, song_data in enumerate(song_list):
+            song_title_with_game = f"[{song_data['game_name']}] {song_data['title']} ({song_data['artist']})"
+            options.append(
+                discord.SelectOption(label=song_title_with_game[:100], value=i)
+            )
+            if i >= 24:
+                break
+        super().__init__(
+            placeholder="Choose a song from result.",
+            min_values=1,
+            max_values=1,
+            options=options,
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        api = MusicGameBotAPI()
+        selected_index = int(self.values[0])
+        selected_song_data = self.song_list[selected_index]
+        result = api.search_songs(
+            selected_song_data["title"],
+            selected_song_data["game_name"],
+            selected_song_data["artist"],
+        )
+        await interaction.response.edit_message(
+            view=DropdownView(DifficultyDropdown(result["songs"]))
+        )
+
+
+class DifficultyDropdown(discord.ui.Select):
+    def __init__(
+        self,
+        song_list: list,
+    ):
+        self.song_list = song_list
+        options = []
+        for chart in song_list[0]["charts"]:
+            difficulty = chart["difficulty"]
+            options.append(discord.SelectOption(label=difficulty, value=difficulty))
+        super().__init__(
+            placeholder="Choose a difficulty.",
+            min_values=1,
+            max_values=1,
+            options=options,
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        song_list = self.song_list
+        song_title = song_list[0]["title"]
+        selected_difficulty = self.values[0]
+        selected_url = None
+        for chart in song_list[0]["charts"]:
+            if chart["difficulty"] == selected_difficulty:
+                selected_url = chart["chart_image_url"]
+                break
+        if selected_url is None:
+            if interaction.response.is_done():
+                return await interaction.followup.edit_message(
+                    message_id=interaction.message.id,
+                    content=f"Chart Not Found ({song_title} [{selected_difficulty}])",
+                    view=None,
+                )
+            else:
+                return await interaction.message.edit(
+                    content=f"Chart Not Found ({song_title} [{selected_difficulty}])",
+                    view=None,
+                )
+
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        driver = webdriver.Chrome(options=options)
+        driver.get(selected_url)
+        driver.set_window_size(5000, 2000)
+        table = driver.find_element(By.CSS_SELECTOR, "table.l td.tbg")
+        file = discord.File(BytesIO(table.screenshot_as_png), filename="image.jpg")
+        driver.quit()
+        if interaction.response.is_done():
+            await interaction.followup.edit_message(
+                message_id=interaction.message.id,
+                content=f"{song_title} [{selected_difficulty}]",
+                attachments=[file],
+                view=None,
+            )
+        else:
+            await interaction.response.edit_message(
+                content=f"{song_title} [{selected_difficulty}]",
+                attachments=[file],
+                view=None,
+            )
 
 
 class DropdownView(discord.ui.View):
